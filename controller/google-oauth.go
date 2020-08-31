d34c7fee2584d3a653eceda1688acaa33c77cfc4
Jerome Le Saux <jeromelesaux@gmail.com>
2020-08-31 14:16:00 +0200

    add: add server url from config file.

--- controller/google-oauth.go
+++ controller/google-oauth.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/jeromelesaux/ethereum-training/config"
 	"github.com/jeromelesaux/ethereum-training/model"
 	"github.com/jeromelesaux/ethereum-training/token"
 	"golang.org/x/oauth2"
@@ -100,11 +101,12 @@ func init() {
 		log.Println("unable to marshal data")
 		return
 	}
-	//serverURL := config.MyConfig.ServerUrl
+	serverURL := config.MyConfig.ServerUrl
+
 	conf = &oauth2.Config{
 		ClientID:     cred.Cid,
 		ClientSecret: cred.Csecret,
-		RedirectURL:  "http://127.0.0.1:8080/auth",
+		RedirectURL:  serverUrl + "/auth",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email", // You have to select your own scope from here -> https://developers.google.com/identity/protocols/googlescopes#google_sign-in
 		},
